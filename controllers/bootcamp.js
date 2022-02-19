3a747a72bec37e5eb1bfda54100bc791a0f5cd30
suganya <suganyakm99@gmail.com>
2022-02-19 22:37:01 +0530

    added middleware

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,24 +1,19 @@
 exports.getAllBootcamps = (req,res,next) => {
     res.json({ status : "SUCCESS" , message: "request processed successfully"});
-    next();
 }
 
 exports.getBootcamp = (req,res,next) => {
     res.json({ status : "SUCCESS" , message: ` fetched data for id ${req.params.id}`});
-    next();
 }
 
 exports.createBootcamp = (req,res,next) => {
     res.json({ status : "SUCCESS" , message: ` added data for id ${req.params.id}`});
-    next();
 }
 
 exports.updateBootcamp = (req,res,next) => {
     res.json({ status : "SUCCESS" , message: ` updated data for id ${req.params.id}`});
-    next();
 }
 
 exports.deleteBootcamp = (req,res,next) => {
     res.json({ status : "SUCCESS" , message: ` deleted data for id ${req.params.id}`});
-    next();
 }
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamp')
+const logger = require('./middleware/logger')
 
-dotenv.config({ path : './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 const app = express();
 
-app.use("/api/v1/bootcamps" , bootcamps);
+app.use(logger);
+app.use("/api/v1/bootcamps", bootcamps);
 
-app.use((req,res)=> {
-console.log("checking execution")
-})
 
-const PORT = process.env.PORT ;
 
-app.listen(PORT , () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
     console.log(`server is running in environment ${process.env.NODE_ENV} in port ${process.env.PORT}`)
 })
